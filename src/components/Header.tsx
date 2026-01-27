6c34c757558648f89cec82063102ddc1482aa0be
OcimarPrado <onprado39@gmail.com>
2026-01-27 15:14:49 -0300

    Atualizando site

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,16 +11,18 @@ const Header: React.FC = () => {
         {/* Logo e Slogan */}
         <div className="logo-container">
           <div className="logo-text"><span>Ocyan</span>-Tech</div>
-          <div className="slogan">Atendimento Inteligente Automatizando Resultados</div>
+          <div className="slogan">Sites profissionais que geram clientes para o seu negócio</div>
         </div>
 
         {/* Menu de navegação */}
         <nav className={`nav-menu ${menuOpen ? "active" : ""}`}>
           <Link to="inicio" smooth={true} duration={500} className="nav-button" onClick={() => setMenuOpen(false)}>Início</Link>
+          <Link to="servico" smooth={true} duration={500} className="nav-button" onClick={() => setMenuOpen(false)}>Serviços</Link>
           <Link to="como-funciona" smooth={true} duration={500} className="nav-button" onClick={() => setMenuOpen(false)}>Como Funciona</Link>
+          <Link to="contato" smooth={true} duration={500} className="nav-button" onClick={() => setMenuOpen(false)}>Contato</Link>
           <Link to="planos" smooth={true} duration={500} className="nav-button" onClick={() => setMenuOpen(false)}>Planos</Link>
           {/*<Link to="contato" smooth={true} duration={500} className="nav-button" onClick={() => setMenuOpen(false)}>Contato</Link>*/}
-          <Link to="planos" smooth={true} duration={500} className="btn-header" onClick={() => setMenuOpen(false)}>Começar Agora</Link>
+          <Link to="planos" smooth={true} duration={500} className="btn-header" onClick={() => setMenuOpen(false)}>Quero meu site</Link>
         </nav>
 
         {/* Menu hambúrguer para mobile */}
