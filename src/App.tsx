9d007fa4e08c6ebbb7852c83e33855955ae64e1a
OcimarPrado <onprado39@gmail.com>
2026-01-30 17:04:37 -0300

    removendo autenticaçãoe e liberando site ao público

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,13 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
-//import { useAuth } from './context/AuthContext';
-import Login from './pages/Login';
 import Home from './pages/Home';
 import Contrato from './pages/Contrato';
-
-// Componente de Rota Protegida
-/*const ProtectedRoute: React.FC<{ children: React.ReactElement }> = ({ children }) => {
-  const { isAuthenticated } = useAuth();
-  
-  if (!isAuthenticated) {
-    return <Navigate to="/login" replace />;
-  }
-  
-  return children;
-};
-*/
-
-const ProtectedRoute: React.FC<{ children: React.ReactElement }> = ({ children }) => {
-  return children;
-};
-
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {/* Rota de Login - Pública */}
-        <Route path="/login" element={<Login />} />
-        
-        {/* Rota Home - Protegida */}
-        <Route 
-          path="/" 
-          element={
-            <ProtectedRoute>
-              <Home />
-            </ProtectedRoute>
-          } 
-        />
-        
-        {/* Rota Contrato - Protegida */}
-        <Route 
-          path="/contrato" 
-          element={
-            <ProtectedRoute>
-              <Contrato />
-            </ProtectedRoute>
-          } 
-        />
-        
-        {/* Rota 404 - Redireciona para home */}
+        <Route path="/" element={<Home />} />
+        <Route path="/contrato" element={<Contrato />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from "react";
+//import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext"; // ajuste o caminho conforme seu projeto
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <AuthProvider>
+  
     <App />
-  </AuthProvider>
+  
 );
