342503b72a8bb571c6c338a42b8f806173f5007d
OcimarPrado <onprado39@gmail.com>
2026-05-18 13:58:50 -0300

    feat: ajuste img social

---
+++ public/robots.tsx
@@ -0,0 +1,12 @@
+const robots = `User-agent: *
+Allow: /
+
+User-agent: facebookexternalhit
+Allow: /
+
+User-agent: Facebot
+Allow: /
+
+Sitemap: https://ocyan-tech.com.br/sitemap.xml`;
+
+export default robots;
